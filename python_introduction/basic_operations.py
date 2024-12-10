6ed0cc4a522b8fe638fd50591694e57700675cb2
danielajah1 <danielajah0909@gmail.com>
2024-12-10 18:14:18 +0100

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+#Define actions
+sum_answers = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print("Sum of", number1, "and", number2, "is", number1 + number2)
+print("Difference of", number1, "and", number2, "is", number1 - number2)
+print("Product of", number1, "and", number2, "is", number1 * number2)
